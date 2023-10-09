a1a63c11c4f24676e8534ba8fcc8948a7112b2c4
qfxla <2690357721@qq.com>
2023-10-09 17:31:06 +0800

    修该go版本

--- common/discovery/discovery.go
+++ common/discovery/discovery.go
@@ -10,14 +10,14 @@ import (
 	"go.etcd.io/etcd/clientv3"
 )
 
-// ServiceDiscovery 服务发现
+//ServiceDiscovery 服务发现
 type ServiceDiscovery struct {
 	cli  *clientv3.Client //etcd client
 	lock sync.Mutex
 	ctx  *context.Context
 }
 
-// NewServiceDiscovery  新建发现服务
+//NewServiceDiscovery  新建发现服务
 func NewServiceDiscovery(ctx *context.Context, endpoints []string) *ServiceDiscovery {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   endpoints,
@@ -33,7 +33,7 @@ func NewServiceDiscovery(ctx *context.Context, endpoints []string) *ServiceDisco
 	}
 }
 
-// WatchService 初始化服务列表和监视
+//WatchService 初始化服务列表和监视
 func (s *ServiceDiscovery) WatchService(prefix string, set, del func(key, value string)) error {
 	//根据前缀获取现有的key
 	resp, err := s.cli.Get(*s.ctx, prefix, clientv3.WithPrefix())
@@ -49,7 +49,7 @@ func (s *ServiceDiscovery) WatchService(prefix string, set, del func(key, value 
 	return nil
 }
 
-// watcher 监听前缀
+//watcher 监听前缀
 func (s *ServiceDiscovery) watcher(prefix string, set, del func(key, value string)) {
 	rch := s.cli.Watch(*s.ctx, prefix, clientv3.WithPrefix())
 	logger.CtxInfof(*s.ctx, "watching prefix:%s now...", prefix)
@@ -65,7 +65,7 @@ func (s *ServiceDiscovery) watcher(prefix string, set, del func(key, value strin
 	}
 }
 
-// Close 关闭服务
+//Close 关闭服务
 func (s *ServiceDiscovery) Close() error {
 	return s.cli.Close()
 }
